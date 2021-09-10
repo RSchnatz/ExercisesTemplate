391d2f9eec0ce27dfb15ad66d7119b0dcfa1939c
RSchnatz <83396573+RSchnatz@users.noreply.github.com>
2021-09-10 02:19:08 +0200

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -12,6 +12,9 @@
 
 TEST_CASE("Ex1", "[example]")
 {
+    REQUIRE(add(0,0) == 0);
+    REQUIRE(add(-1,2) == 1);
+    REQUIRE(add(2,-1) == 1);
     REQUIRE(add(1,1) == 2);
 }
 
@@ -21,6 +24,10 @@ TEST_CASE("Ex1", "[example]")
 
 TEST_CASE("Ex2", "[example]")
 {
-    REQUIRE(mul(3,3) == 9);
+    REQUIRE(mul(0,0) == 0);
+    REQUIRE(mul(1,0) == 0);
+    REQUIRE(mul(2,2) == 4);
+    REQUIRE(mul(-2,-2) == 4);
+    REQUIRE(mul(-2,2) == -4); 
 }
 
